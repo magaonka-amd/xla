184fe1b27fd801052098e66038c177dcffb1b6fb
Pavel Emeliyanenko <pavel.emeliyanenko@amd.com>
2025-07-15 11:43:52 -0400

    added scheduling fix

--- xla/service/gpu/compile_module_to_llvm_ir.cc
+++ xla/service/gpu/compile_module_to_llvm_ir.cc
@@ -264,12 +264,18 @@ absl::StatusOr<std::unique_ptr<BufferAssignment>> RunBufferAssignment(
           ? std::optional<BufferValue::Color>(kTempBufferMemorySpaceColor)
           : std::nullopt;
 
+  using HloOrderingPtr = std::unique_ptr<HloOrdering>;
+  auto hlo_ordering = 
+      options.xla_gpu_graph_enable_concurrent_region() 
+        ? static_cast<HloOrderingPtr>(
+                     std::make_unique<DependencyHloOrdering>(module))
+        : static_cast<HloOrderingPtr>(
+              std::make_unique<SequentialHloOrdering>(module->schedule()));
+
   TF_ASSIGN_OR_RETURN(
       std::unique_ptr<BufferAssignment> buffer_assignment,
       BufferAssigner::Run(
-          module, 
-          std::make_unique<DependencyHloOrdering>(module),
-          //std::make_unique<SequentialHloOrdering>(module->schedule()),
+          module, std::move(hlo_ordering),
           buffer_size_bytes_function, alias_info,
           /*color_alignment=*/
           [](LogicalBuffer::Color) { return kXlaAllocatedBufferAlignBytes; },
