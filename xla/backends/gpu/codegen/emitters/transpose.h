716965034a096427ffe256e4df71d19702484b2a
Pavel Emeliyanenko <pavel.emeliyanenko@amd.com>
2025-12-15 12:17:26 +0000

    Revert "Port PackedTranspose performance regression fix (#441)"
    This reverts commit ed508c3768b6ed047ef148ebedf0f2bc2953c711.

--- xla/backends/gpu/codegen/emitters/transpose.h
+++ xla/backends/gpu/codegen/emitters/transpose.h
@@ -201,24 +201,19 @@ class TransposeFusion : public TransposeFusionBase {
 //    slice of shared memory.
 //
 // 5. Every GPU block gets a single 64 x 10 x 6 x bf16 tile.
-//    The tile is read by `num_shmem_groups_per_block` shmem groups.
-//    Let's assume that there are 4 shmem groups per block. In this case, on
-//    every iteration each shmem group will read 10 x 6 x bf16 elements, i.e.
-//    every thread (30 out of 32) performs a vector load of 2 x bf16 and stores
-//    it to the shared memory. In total, there will be 16 iterations performed
-//    by each block.
-//
-//    Note: When the hardware warp size equals kNumShmemBanks (32), then
-//    num_shmem_groups_per_block equals the number of warps per block. This is
-//    the case for NVIDIA GPUs, but not always for AMD GPUs where warp size
-//    can differ (64).
+//    The tile is read by `num_warps_per_block` warps.
+//    Let's assume that there are 4 warps per block. In this case, on every
+//    iteration each warp will read 10 x 6 x bf16 elements, i.e. every thread
+//    (30 out of 32) performs a vector load of 2 x bf16 and stores it to the
+//    shared memory. In total, there will be 16 iterations performed by each
+//    block.
 //
 //    The following code snippet shows how the data is read from the input
 //    tensor into the shared memory:
 //
-//    for I = 0 to CEIL(shmem_rows, num_shmem_groups_per_block):
+//    for I = 0 to CEIL(shmem_rows, num_warps_per_block):
 //      for J = 0 to VECTOR_SIZE:
-//        ROW = SHMEM_GROUP_ID + NUM_SHMEM_GROUPS * I
+//        ROW = WARP_ID + NUM_WARPS * I
 //        COL = LANE_ID * VECTOR_SIZE + J
 //        SHMEM[ROW, COL] = INPUT[ROW, COL / 10, COL % 10]
 //
@@ -227,7 +222,7 @@ class TransposeFusion : public TransposeFusionBase {
 // 6. Each thread reads a VECTOR_SIZE x VECTOR_SIZE x bf16 tile from the shared
 //    memory and performs the write of each of the columns of the tile.
 //
-//    for I = 0 to CEIL(shmem_cols, VECTOR_SIZE * num_shmem_groups_per_block):
+//    for I = 0 to CEIL(shmem_cols, VECTOR_SIZE * num_warps_per_block):
 //      VECTOR_2D = arith.constant dense<0>
 //        : vector<VECTOR_SIZE x VECTOR_SIZE x bf16>
 //      for J = 0 to VECTOR_SIZE:
@@ -241,7 +236,7 @@ class PackedTranspose : public TransposeFusionBase {
   explicit PackedTranspose(const HloFusionAnalysis& analysis,
                            const TransposeSpec& spec,
                            absl::Span<const int64_t> output_block_tile,
-                           int64_t num_shmem_groups,
+                           int64_t num_warps,
                            SymbolicExprContext* symbolic_expr_context);
 
   LaunchDimensions launch_dimensions() const override;
@@ -291,10 +286,8 @@ class PackedTranspose : public TransposeFusionBase {
   // Vector size in elements.
   int64_t vector_size_;
 
-  // Number of shmem groups per block. Each shmem group consists of 32 threads
-  // (kNumShmemBanks), chosen to match the number of shared memory banks for
-  // optimal memory access patterns. This is independent of hardware warp size.
-  int64_t num_shmem_groups_per_block_;
+  // Number of warps per block.
+  int64_t num_warps_per_block_;
 
   // Tile sizes for the canonicalical dimensions
   // [T2, A, T1, 1] -> [T1, A, T2, 1].
